d56d0e404f985d029dba9ff7d24816e002f61254
Michael Vetter <jubalh@iodoru.org>
2019-01-08 16:51:54 +0100

    Add help option and description to all subcommands

--- src/options.c
+++ src/options.c
@@ -6,72 +6,183 @@
 #include <unistd.h> // access(), getopt()
 #include "commands.h"
 
+static int parse_newlog(int argc, char *argv[])
+{
+	int opt;
+	int dummydata = 0;
+
+	while ((opt = getopt(argc, argv, "he")) != -1)
+	{
+		switch (opt)
+		{
+			// help
+			case 'h':
+				printf("roshi \e[3mfile_name\e[0m newlog: creates a new training log file.\n\n");
+				printf("-e: Fill database with example values\n");
+				printf("-h: Display this help\n");
+				exit(EXIT_SUCCESS);
+				break;
+			// example data
+			case 'e':
+				dummydata = 1;
+				break;
+			// invalid option
+			default:
+				return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
+		}
+	}
+
+	if (access(argv[0], F_OK) >= 0)
+	{
+		printf("Aborting: File already exists.\n");
+		return 2;
+	}
+
+	cmd_newlog(argv[0], dummydata);
+}
+
+static int parse_show(int argc, char *argv[])
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "h")) != -1)
+	{
+		switch (opt)
+		{
+			// help
+			case 'h':
+				printf("roshi \e[3mfile_name\e[0m show \e[3msession_name\e[0m: displays a session.\n\n");
+				printf("-h: Display this help\n");
+				exit(EXIT_SUCCESS);
+				break;
+			// invalid option
+			default:
+				return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
+		}
+	}
+
+	cmd_show(argv[0], argv[2]);
+}
+
+static int parse_add(int argc, char *argv[])
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "h")) != -1)
+	{
+		switch (opt)
+		{
+			// help
+			case 'h':
+				printf("roshi \e[3mfile_name\e[0m add: adds a session.\n\n");
+				printf("-h: Display this help\n");
+				exit(EXIT_SUCCESS);
+				break;
+			// invalid option
+			default:
+				return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
+		}
+	}
+
+	cmd_add(argv[0]);
+}
+
+static int parse_list(int argc, char *argv[])
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "h")) != -1)
+	{
+		switch (opt)
+		{
+			// help
+			case 'h':
+				printf("roshi \e[3mfile_name\e[0m list: lists all logged sessions.\n\n");
+				printf("-h: Display this help\n");
+				exit(EXIT_SUCCESS);
+				break;
+			// invalid option
+			default:
+				return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
+		}
+	}
+
+	cmd_list(argv[0]);
+}
+
+static int parse_analyze(int argc, char *argv[])
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "h")) != -1)
+	{
+		switch (opt)
+		{
+			// help
+			case 'h':
+				printf("roshi \e[3mfile_name\e[0m analyze special analyzation functions.\n\n");
+				printf("-h: Display this help\n");
+				exit(EXIT_SUCCESS);
+				break;
+			// invalid option
+			default:
+				return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
+		}
+	}
+
+	cmd_analyze(argv[0]);
+}
+
+static int parse_pr(int argc, char *argv[])
+{
+	int opt;
+
+	while ((opt = getopt(argc, argv, "h")) != -1)
+	{
+		switch (opt)
+		{
+			// help
+			case 'h':
+				printf("roshi \e[3mfile_name\e[0m pr: lists all personal records.\n\n");
+				printf("-h: Display this help\n");
+				exit(EXIT_SUCCESS);
+				break;
+			// invalid option
+			default:
+				return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
+		}
+	}
+
+	cmd_pr(argv[0]);
+}
+
 int parse_options(int argc, char *argv[])
 {
-	//-----------------------
-	// NEWLOG
-    if (strcmp(argv[1], "newlog") == 0)
-    {
-		int opt;
-		int dummydata = 0;
-
-		while ((opt = getopt(argc, argv, "he")) != -1)
+	if (strcmp(argv[1], "newlog") == 0)
+	{
+		parse_newlog(argc, argv);
+	} else {
+		// if db doesnt exist or no further argument
+		if ( (access(argv[0], F_OK) == -1) || argc < 2)
 		{
-			switch (opt)
-			{
-				case 'h':
-					printf("newlog creates a new training log file.\n\n");
-					printf("-e: Fill database with example values\n");
-					printf("-h: Display this help\n");
-					exit(EXIT_SUCCESS);
-					break;
-				case 'e':
-					dummydata = 1;
-					break;
-				// invalid option
-				default:
-					return 2; // actually EXIT_FAILURE, but we don't want usage to be displayed
-			}
+			return EXIT_FAILURE;
 		}
 
-        cmd_newlog(argv[0], dummydata);
-    } else {
-        // if db doesnt exist or no further argument
-        if ( (access(argv[0], F_OK) == -1) || argc < 3)
-        {
-            return EXIT_FAILURE;
-        }
+		if (strcmp(argv[1], "show") == 0)
+		{
+			parse_show(argc, argv);
+		} else if (strcmp(argv[1], "add") == 0) {
+			parse_add(argc, argv);
+		} else if (strcmp(argv[1], "list") == 0) {
+			parse_list(argc, argv);
+		} else if (strcmp(argv[1], "analyze") == 0) {
+			parse_analyze(argc, argv);
+		} else if (strcmp(argv[1], "pr") == 0) {
+			parse_pr(argc, argv);
+		} else {
+			return EXIT_FAILURE;
+		}
+	}
 
-		//-----------------------
-		// SHOW
-        if (strcmp(argv[1], "show") == 0)
-        {
-            if (argc < 4)
-            {
-                return EXIT_FAILURE;
-            } else {
-                cmd_show(argv[0], argv[2]);
-            }
-		//-----------------------
-		// ADD
-        } else if (strcmp(argv[1], "add") == 0) {
-            cmd_add(argv[0]);
-		//-----------------------
-		// LIST
-        } else if (strcmp(argv[1], "list") == 0) {
-            cmd_list(argv[0]);
-		//-----------------------
-		// ANALYZE
-        } else if (strcmp(argv[1], "analyze") == 0) {
-            cmd_analyze(argv[0]);
-		//-----------------------
-		// PR
-        } else if (strcmp(argv[1], "pr") == 0) {
-            cmd_pr(argv[0]);
-        } else {
-            return EXIT_FAILURE;
-        }
-    }
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
