09c2f9fa7f8eb5d938f7cb4ab0ff50b43fa99d2c
Michael Vetter <jubalh@iodoru.org>
2019-01-26 11:19:07 +0100

    Initial configuration file support
    So far just reading a test value from a file in cwd.

---
+++ src/config.h
@@ -0,0 +1,7 @@
+// vim: noexpandtab:ts=4:sts=4:sw=4
+
+#pragma once
+
+struct roshi_config {
+	char *test;
+};
--- src/main.c
+++ src/main.c
@@ -2,9 +2,33 @@
 
 #include <stdio.h> // printf()
 #include <stdlib.h> // EXIT_SUCCESS
-#include "options.h"
+#include <confuse.h> // cfg_*
+#include "options.h" // parse_options()
+#include "config.h" // roshi_config
 
 #define VERSION "0.0"
+#define CONF_FILE "roshi.conf"
+
+static void read_config(struct roshi_config *config)
+{
+	config->test = NULL;
+
+	cfg_opt_t cfgopt[] = {
+		CFG_SIMPLE_STR("test", &config->test),
+		CFG_END()
+	};
+
+	cfg_t *cfg;
+	cfg = cfg_init(cfgopt, CFGF_IGNORE_UNKNOWN);
+	cfg_parse(cfg, CONF_FILE);
+	cfg_free(cfg);
+}
+
+static void free_config(struct roshi_config *config)
+{
+	free(config->test);
+	config->test = NULL;
+}
 
 static void print_usage()
 {
@@ -24,11 +48,17 @@ static void print_usage()
 
 int main(int argc, char *argv[])
 {
+	struct roshi_config config;
+	read_config(&config);
+
 	if (argc < 3)
 	{
 		print_usage();
 		return EXIT_SUCCESS;
 	}
 
-	return parse_options(argc-1, argv+1);
+	int ret = parse_options(argc-1, argv+1);
+	free_config(&config);
+
+	return ret;
 }
