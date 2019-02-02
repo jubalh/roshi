f57aa009fe729c6fe341b55ca85871b6898da0d4
Michael Vetter <jubalh@iodoru.org>
2019-02-02 20:53:54 +0100

    Config loader: get rid of global var
    Let's rather return char pointers for the actual path.

--- src/config.c
+++ src/config.c
@@ -9,58 +9,71 @@
 
 #define CONF_FILE "roshi.conf"
 
-static char* conf_path = NULL;
-
-static bool set_conf_path(char *fn)
+// checks whether config is readable at fn
+// returns full path if true. NULL if not.
+static char* set_conf_path(char *fn)
 {
 	if (access(fn, F_OK) >= 0)
 	{
-		conf_path = strdup(fn);
-		return true;
+		return strdup(fn);
 	}
-	return false;
+	return NULL;
 }
 
-static bool choose_config(struct roshi_config *config)
+// chooses config after following prio:
+// 1. local, 2. XDG_CONFIG_HOME 3. ~/.config
+// returns the path that got hit first
+static char* choose_config(struct roshi_config *config)
 {
-	if (set_conf_path(CONF_FILE))
-		return true;
+	char *path;
+	path = set_conf_path(CONF_FILE);
+	if (path != NULL)
+		return path;
 
 	char tmp_path[4096];
 	char *p = getenv("XDG_CONFIG_HOME");
 	if (p != NULL)
 	{
 		sprintf(tmp_path, "%s/roshi/config", p);
-		if (set_conf_path(tmp_path))
-			return true;
+		path = set_conf_path(tmp_path);
+		if (path != NULL)
+			return path;
 	}
 
 	p = getenv("HOME");
 	if (p != NULL)
 	{
 		sprintf(tmp_path, "%s/.config/roshi/config", p);
-		if (set_conf_path(tmp_path))
-			return true;
+		path = set_conf_path(tmp_path);
+		if (path != NULL)
+			return path;
 	}
 
-	return false;
+	return NULL;
+}
+
+static void parse_config(struct roshi_config *config, char *path)
+{
+	cfg_opt_t cfgopt[] = {
+		CFG_SIMPLE_STR("test", &config->test),
+		CFG_END()
+	};
+
+	cfg_t *cfg;
+	cfg = cfg_init(cfgopt, CFGF_IGNORE_UNKNOWN);
+	cfg_parse(cfg, path);
+	cfg_free(cfg);
 }
 
 void read_config(struct roshi_config *config)
 {
+	// init struct
 	config->test = NULL;
 
-	if (choose_config(config))
+	char *conf_path = choose_config(config);
+	if (conf_path != NULL)
 	{
-		cfg_opt_t cfgopt[] = {
-			CFG_SIMPLE_STR("test", &config->test),
-			CFG_END()
-		};
-
-		cfg_t *cfg;
-		cfg = cfg_init(cfgopt, CFGF_IGNORE_UNKNOWN);
-		cfg_parse(cfg, conf_path);
-		cfg_free(cfg);
+		parse_config(config, conf_path);
 		free(conf_path);
 	}
 }
