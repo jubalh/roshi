68a05412aafd3aa290228a4da634e78b7d775998
Michael Vetter <jubalh@iodoru.org>
2019-02-02 20:19:14 +0100

    Read config from various locations
    Choose config using following prios:
    1 same path as executable
    2 XDG_CONFI_HOME/roshi/config
    3 ~/home/.config/roshi/config

--- src/config.c
+++ src/config.c
@@ -1,17 +1,56 @@
 // vim: noexpandtab:ts=4:sts=4:sw=4
 
 #include <stdlib.h> // free() 
+#include <stdbool.h> // bool
+#include <string.h> // strdup()
 #include <unistd.h> // access()
 #include <confuse.h> // cfg_*
 #include "config.h" // roshi_config
 
 #define CONF_FILE "roshi.conf"
 
+static char* conf_path = NULL;
+
+static bool set_conf_path(char *fn)
+{
+	if (access(fn, F_OK) >= 0)
+	{
+		conf_path = strdup(fn);
+		return true;
+	}
+	return false;
+}
+
+static bool choose_config(struct roshi_config *config)
+{
+	if (set_conf_path(CONF_FILE))
+		return true;
+
+	char tmp_path[4096];
+	char *p = getenv("XDG_CONFIG_HOME");
+	if (p != NULL)
+	{
+		sprintf(tmp_path, "%s/roshi/config", p);
+		if (set_conf_path(tmp_path))
+			return true;
+	}
+
+	p = getenv("HOME");
+	if (p != NULL)
+	{
+		sprintf(tmp_path, "%s/.config/roshi/config", p);
+		if (set_conf_path(tmp_path))
+			return true;
+	}
+
+	return false;
+}
+
 void read_config(struct roshi_config *config)
 {
 	config->test = NULL;
 
-	if (access(CONF_FILE, F_OK) >= 0)
+	if (choose_config(config))
 	{
 		cfg_opt_t cfgopt[] = {
 			CFG_SIMPLE_STR("test", &config->test),
@@ -20,8 +59,9 @@ void read_config(struct roshi_config *config)
 
 		cfg_t *cfg;
 		cfg = cfg_init(cfgopt, CFGF_IGNORE_UNKNOWN);
-		cfg_parse(cfg, CONF_FILE);
+		cfg_parse(cfg, conf_path);
 		cfg_free(cfg);
+		free(conf_path);
 	}
 }
 
